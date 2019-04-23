3454ca45b0b38cb74439b01d7f4a1cf1ad2a153e
Jun Jie Ng <jun@backbase.com>
2019-04-23 18:21:55 +0800

    Make test fail

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,12 +3,13 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
-public class App
-{
+public class App {
 
     private final String message = "Hello World!";
 
-    public App() {}
+    public App() {
+        throw new RuntimeException("Problem!");
+    }
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
