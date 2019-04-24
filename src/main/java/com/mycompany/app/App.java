60b995036bcdb92dfd90f5847bd4e467ebf43fb2
Jun Jie Ng <jun@backbase.com>
2019-04-24 09:49:44 +0800

    Remove no arg constructor

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,8 +7,6 @@ public class App {
 
     private final String message = "Hello World!";
 
-    public App() {}
-
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
     }
